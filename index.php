e71c7a08111cd8f2f873f8599a0f2a3044332ad4
KantoAndrianina <kanto.andrianina@yahoo.com>
2024-03-19 14:33:25 +0300

    analytics-1

--- index.php
+++ index.php
@@ -15,6 +15,15 @@ else {
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-9WJG41MZCM"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-9WJG41MZCM');
+		</script>
 		<title>Welcome to nita's online grocery</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
