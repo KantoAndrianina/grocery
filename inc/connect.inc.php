35556670f5ca01107104a46cf28c5558c79208b6
KantoAndrianina <kanto.andrianina@yahoo.com>
2024-03-19 14:18:01 +0300

    con

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,5 +1,5 @@
 <?php 
 	// $con = mysqli_connect("localhost","root","root", "grocerydb") or die("Couldn't connect to SQL server");
-	$con = mysqli_connect("mysql-andrianina24.alwaysdata.net","320578","Andrianina2002!", "andrianina24_grocerydb") or die("Couldn't connect to SQL server");
+	$con = mysqli_connect("mysql-andrianina24.alwaysdata.net","320578","Andrianina2002!", "andrianina24_grocery") or die("Couldn't connect to SQL server");
 
 ?>
